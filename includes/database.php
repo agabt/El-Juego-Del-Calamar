d9b2963ad2534f70f8ef500a8ff4d360d68ecfac
jbelmonteg <jbelmonteg@esliceu.net>
2022-04-06 11:11:18 +0200

    Database creat
    He creado database y modificado head

---
+++ includes/database.php
@@ -0,0 +1,13 @@
+<?php
+
+$dbh = mysqli_connect('localhost', '', '');
+if (!$dbh) {
+echo "Error db_connect: error en mysqli_connect user: (mal usuario/password?)<br>";
+die;
+}
+
+if (!mysqli_select_db($dbh, 'test')) {
+echo "Error db_connect: error en mysqli_select_db: _BBDD_NAME<br>";
+die;
+}
+?>
