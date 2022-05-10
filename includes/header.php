c426cb1a4e764fbd5dd9571044ac04c2ac8417a5
Anthony G <agabt@esliceu.net>
2022-05-10 11:33:05 +0200

    Canvis vista amb CSS (i syles WIP)

--- includes/header.php
+++ includes/header.php
@@ -1,9 +1,9 @@
-<header>
-    <a href="index.php"> Índex </a>
-    <a href="empleats.php"> Empleats </a>
-    <a href="jocs.php"> Jocs </a>
-    <a href="jugadors.php"> Jugadors </a>
-    <a href="temporades.php"> Temporades </a>
-    <a href="insertar un nou jugador.php"> Insertar un nou jugador </a>
-    <a href="borrar jugador.php"> Borrar un jugador </a>
+<header style="text-align: center; margin: 0.5rem;">
+    <a style="margin: 1rem;" href="index.php"> Índex </a>
+    <a style="margin: 1rem;" href="empleats.php"> Empleats </a>
+    <a style="margin: 1rem;" href="jocs.php"> Jocs </a>
+    <a style="margin: 1rem;" href="jugadors.php"> Jugadors </a>
+    <a style="margin: 1rem;" href="temporades.php"> Temporades </a>
+    <a style="margin: 1rem;" href="insertar un nou jugador.php"> Insertar un nou jugador </a>
+    <a style="margin: 1rem;" href="borrar jugador.php"> Borrar un jugador </a>
 </header>
