74b644d7939a9f254c169576ce09d8fcc0175514
Anthony G <agabt@esliceu.net>
2022-06-07 10:58:08 +0200

    intent CSS

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,3 @@
 <footer>
-     Copyright <a href="https://google.com">Anthony Gab Terescu</a> & <a href="https://google.com">Joan Belmonte Gari </a>
+     Copyright <a href="https://instagram.com/anthonypizza10/">Anthony Gab Terescu</a> & <a href="https://instagram.com/belmonte_04/">Joan Belmonte Gari </a>
 </footer>
