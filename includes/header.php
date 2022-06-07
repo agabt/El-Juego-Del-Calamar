73a21562498ceb92614694b7131d83569d778f8a
jbelmonteg <jbelmonteg@esliceu.net>
2022-06-07 11:49:52 +0200

    Logo

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,11 @@
 <header class="mb-4 mt-4">
-    <a class="header_a" href="/El-Juego-Del-Calamar/index.php"> Índex </a>
-    <a class="header_a" href="/El-Juego-Del-Calamar/empleats.php"> Empleats </a>
-    <a class="header_a" href="/El-Juego-Del-Calamar/jocs.php"> Jocs </a>
-    <a class="header_a" href="/El-Juego-Del-Calamar/jugadors.php"> Jugadors </a>
-    <a class="header_a" href="/El-Juego-Del-Calamar/temporades.php"> Temporades </a>
+    <div class="div_header">
+        <img src="/El-Juego-Del-Calamar/imatges/El Juego Del Calamar Logo.png" alt="Juego del calamar logo" class="logo_header">
+        <a class="header_a" href="/El-Juego-Del-Calamar/index.php"> Índex </a>
+        <a class="header_a" href="/El-Juego-Del-Calamar/empleats.php"> Empleats </a>
+        <a class="header_a" href="/El-Juego-Del-Calamar/jocs.php"> Jocs </a>
+        <a class="header_a" href="/El-Juego-Del-Calamar/jugadors.php"> Jugadors </a>
+        <a class="header_a" href="/El-Juego-Del-Calamar/temporades.php"> Temporades </a>
+    </div>
+
 </header>
